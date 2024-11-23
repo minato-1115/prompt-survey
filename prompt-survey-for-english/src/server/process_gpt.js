f07d2ffc630b9db59a123e357e15a49bf598e8d4
minato-1115 <akasatana6789hama@gmail.com>
2024-11-24 00:50:21 +0900

    heroku にdeploy

--- prompt-survey-for-english/src/server/process_gpt.js
+++ prompt-survey-for-english/src/server/process_gpt.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const cors = require("cors")
-
+const path = require("path")
 require("dotenv").config()
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname,"dist")))
 app.get("/api/chat", (req, res) => {
   res.send("このエンドポイントはGETリクエストには対応していません。POSTリクエストを使用してください。");
 });
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 app.post("/api/chat",async(req,res)=>{
   //console.log("リクエストを受信",req.body)
   const {messages} = req.body;
